8f24aa89907432d2b4f9c05a1d7fec5dc3807932
Zisis Vakras <zisis.vakras@protonmail.com>
2023-01-08 21:45:05 +0200

    fast sum_bit

--- src/maps.c
+++ src/maps.c
@@ -85,11 +85,12 @@ void join_map(int* map1, int* map2, int map_size) {
 }
 
 int sum_bit(int* map, int map_size) {
+    if(map == NULL) return 0;
     int count = 0;
     for (int i = 0 ; i < map_size ; ++i) {
         if (map[i] == 0) continue;
         for (int j = 0 ; j < 32 ; j++) {
-            count += (map[i] >> j) & 1;
+            if ((map[i] >> j) & 1) count++;
         }
     }
     return count;
